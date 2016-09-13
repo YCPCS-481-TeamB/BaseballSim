12280e20567c43e6e5591caf9b17a4638a1c6801
Scott Fitzpatrick <no>
2016-09-13 15:48:39 -0400

    Split player table into stats and attributes tables for less table complexity

--- Database/CreateTables.sql
+++ Database/CreateTables.sql
@@ -5,13 +5,32 @@ CREATE TYPE player_position AS ENUM ('pitcher', 'catcher', 'first_baseman', 'sec
 CREATE TYPE dominant_arm AS ENUM ('left', 'right');
 
 CREATE TABLE players (
-    id PRIMARY KEY, firstname VARCHAR (255), lastname (255), position player_position,
-		--Stats
+    id PRIMARY KEY, firstname VARCHAR (255), lastname (255), position player_position, team_id INTEGER
+);
+
+CREATE TABLE attributes (
+	player_id INTEGER,
+		--Pitching attributes
 	technique INTEGER, pitch_speed INTEGER, endurance INTEGER,
+		--Batting attributes
 	contact INTEGER, swing_speed INTEGER, bat_power INTEGER,
+		--Fielding attributes
 	catching INTEGER, throwing INTEGER, awareness INTEGER,
-	speed INTEGER, clutch INTEGER, arm dominant_arm,
-	team_id INTEGER
+		--Misc attributes
+	speed INTEGER, clutch INTEGER, arm dominant_arm
+);
+
+CREATE TABLE stats (
+	player_id INTEGER,
+		--Fieldplayer Stats
+	hits INTEGER, doubles INTEGER, games_played INTEGER, at_bats INTEGER,
+	runs INTEGER, triples INTEGER, homeruns INTEGER, runs_batted_in INTEGER,
+	stolen_bases INTEGER, on_base_percentage DOUBLE, batting_average DOUBLE,
+	strikeouts INTEGER, walks INTEGER
+		--Pitching Stats
+	wins INTEGER, losses INTEGER, hits_allowed INTEGER, innings_pitched FLOAT,
+	runs_allowed INTEGER, walks_allowed INTEGER, earned_runs INTEGER,
+	earned_run_average DOUBLE, strikeouts_thrown INTEGER, saves INTEGER
 );
 
 CREATE TABLE teams (id PRIMARY KEY, name VARCHAR(255), league_id INTEGER);
