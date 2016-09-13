d335f99dbcaf9eaf6a2afc1a29070262db703f2e
Scott Fitzpatrick <no>
2016-09-13 12:50:09 -0400

    Added Stats to player tables in database

--- Database/CreateTables.sql
+++ Database/CreateTables.sql
@@ -1,9 +1,16 @@
-CREATE TABLE users (id PRIMARY KEY, username VARCHAR(255), password VARCHAR(255), firstname VARCHAR(255), lastname VARCHAR(255), email VARCHAR(255));
+CREATE TABLE users (id PRIMARY KEY, username VARCHAR(255), password VARCHAR(255), firstname VARCHAR(255), lastname VARCHAR(255), email VARCHAR(255), date_created TIMESTAMP DEFAULT NOW());
 
 CREATE TYPE player_position AS ENUM ('pitcher', 'catcher', 'first_baseman', 'second_baseman', 'third_baseman','shortstop', 'left_fielder', 'center_fielder', 'right_fielder');
 
+CREATE TYPE dominant_arm AS ENUM ('left', 'right');
+
 CREATE TABLE players (
-    id PRIMARY KEY, firstname VARCHAR (255), lastname (255), position player_position
+    id PRIMARY KEY, firstname VARCHAR (255), lastname (255), position player_position,
+		--Stats
+	technique INTEGER, pitch_speed INTEGER, endurance INTEGER,
+	contact INTEGER, swing_speed INTEGER, bat_power INTEGER,
+	catching INTEGER, throwing INTEGER, awareness INTEGER,
+	speed INTEGER, clutch INTEGER, arm dominant_arm
 );
 
 CREATE TABLE teams (id PRIMARY KEY, name VARCHAR(255));
