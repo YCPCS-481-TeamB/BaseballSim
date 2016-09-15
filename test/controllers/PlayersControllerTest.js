0107b3200c68a9c98e94fd5defce26e4d6ccc9f0
Cooper Luetje <koopaluigi@hotmail.com>
2016-09-15 19:31:40 +0000

    Updated test folder and PlayersControllerTest

--- test/controllers/PlayersControllerTest.js
+++ test/controllers/PlayersControllerTest.js
@@ -1,31 +1,37 @@
 var assert = require('assert');
-var PlayersController = require('../Controller/PlayersController');
+var PlayersController = require('../../Controller/PlayersController');
+
+//Player Setup
+var firstname = 'John';
+var lastname = 'Doe';
+var position = 'Pitcher';
+var team_id = 1;
+var player = PlayersController.createPlayer(firstname, lastname, position, team_id);
 
 //Run Tests
-test_create_player();
-test_create_random_player();
+//test_create_player();
+//test_create_random_player();
 //test_delete_player();
-test_get_players();
-test_get_players_by_id();
+//test_get_players();
+//test_get_players_by_id();
 
 //Tests the createPlayer function
 function test_create_player()
 {
-    var player = PlayersController.createPlayer('John', 'Doe', 'Pitcher', 1);
     assert.notEqual(null, player);
 }
 
 //Tests the createRandomPlayer function
 function test_create_random_player()
 {
-    var player = PlayersController.createRandomPlayer(1);
-    assert.notEqual(null, player);
+    var randomPlayer = PlayersController.createRandomPlayer(team_id);
+    assert.notEqual(null, randomPlayer);
 }
 
 //Tests the deletePlayer function
 function test_delete_player()
 {
-    var player = PlayersController.deletePlayer(1);
+    var player = PlayersController.deletePlayer(player.id);
     assert.notEqual(null, player);
 }
 
@@ -41,6 +47,6 @@ function test_get_players()
 //Tests the getPlayersById function
 function test_get_players_by_id()
 {
-    var player = PlayersController.getPlayersById(1);
+    var player = PlayersController.getPlayersById(player.id);
     assert.notEqual(null, player);
 }
