a154007fa24b0623d494a335f7d57ad0cf9b2596
Scott Fitzpatrick <no>
2016-09-15 13:11:26 -0400

    Added get and getById functions to leagues and teams api route, also league and team contollers

--- routes/leagues.js
+++ routes/leagues.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
 
+var LeaguesController = require('./../Controller/LeaguesController');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.status(200).json({success: true, message: "Leagues"});
+	LeaguesController.getLeagues().then(function(data)){
+		res.status(200).json({leagues: data});
+	}).catch(function(err){
+		res.status(200).json({success: false, message:""+ err});
+	});
+    //res.status(200).json({success: true, message: "Leagues"});
 });
 
+
+/*
+ * Retrieves the league from the database with the given id value
+ * @params id - the id of the league
+ */
+router.get('/:id', function(req, res, next){
+	var id = req.params.id;
+	LeaguesController.getLeagueById(id).then(function(data){
+		res.status(200).json({id: id, league:data});	
+	}).catch(function(err){
+		res.status(200).json({success: false, id: id, message:""+ err});
+	});
+});
+ 
+ 
 module.exports = router;
--- routes/teams.js
+++ routes/teams.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
+var TeamsController = require('./../Controller/TeamsController');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.status(200).json({success: true, message: "Teams"});
+	TeamsController.getTeams().then(function(data) {
+		res.status(200).json({teams: data});
+	}).catch(function(err){
+		res.status(200).json({success: false, message:""+ err});
+	});
+    /* res.status(200).json({success: true, message: "Teams"}); */
 });
+
+/*
+ * Retrieves the team from the database with the given id value
+ * @params id = The id of the team
+ */
+ 
+ router.get('/:id', function(req, res, next){
+	 var id = req.params.id;
+	 TeamsController.getTeamById(id).then(function(data){
+		 res.status(200).json({id: id, team: data});
+	 }).catch(function(err){
+		 res.status(200).json({success: false, id: id, message:""+ err});
+	 });
+ });
 
 module.exports = router;
