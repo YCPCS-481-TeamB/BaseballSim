c473620fed247e250f12ff15f335d913f7983063
Scott Fitzpatrick <sfitzpa4@ycp.edu>
2016-09-19 00:36:46 -0400

    Added createleaguebyid in leaguecontroller as well as delete league and team by id

--- Controller/LeaguesController.js
+++ Controller/LeaguesController.js
@@ -10,8 +10,22 @@ exports.getLeagues = function() {
 
 exports.getLeagueById = function(id){
 	return new Promise(function(resolve, reject){
-		DatabaseController.query("SELECT * from league WHERE id = $1", [id]).then(function(data){
+		DatabaseController.query("SELECT * from leagues WHERE id = $1", [id]).then(function(data){
 			resolve(data.rows);
 		});
 	});
+}
+
+exports.createLeague = function(league_name, league_id) {
+	return DatabaseController.query("INSERT INTO leagues (name, league_id) VALUES($1, $2)", [league_name, league_id]);
+}
+
+exports.deleteLeagueById = function(id) {
+	return new Promise(function(resolve, reject){
+		DatabaseController.query("DELETE FROM leagues WHERE id = $1 RETURNING *;", [id]).then(function(data) {
+			resolve({league: data.rows});
+		}).catch(function (err) {
+			reject(err);
+		});
+	}
 }
