c10a929e7e30f7c02b467ea941443ffee1353282
Brandon Walton <bfwalton@comcast.net>
2016-09-23 14:33:10 -0400

    Added angular game service

---
+++ public/javascripts/service/GameService.js
@@ -0,0 +1,14 @@
+var GameService = App.service('GameService', function($http){
+
+    this.getAll = function(){
+        return $http.get('/api/games');
+    }
+
+    this.create= function(team_id, team2_id, field_id, league_id){
+        return $http.post('/api/games', {team1_id: team1_id, team2_id: team2_id, field_id: field_id, league_id: league_id});
+    }
+
+    this.deleteById = function(id){
+        return $http.delete('/api/games/'+id);
+    }
+});
