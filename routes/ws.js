81c8184e861761ca089428019bb8333775b6273c
Brandon Walton <bfwalton@comcast.net>
2016-11-25 14:25:16 -0500

    Updated fieldDisplay and messed with the ws a little.

--- routes/ws.js
+++ routes/ws.js
@@ -1,9 +1,10 @@
+var ws = require('ws');
 var express = require('express');
 var router = express.Router();
 
-//router.get('/', function(req, res, next){
-//    res.status(200).render('api', {title: "API Endpoint Listing", loggedIn: false});
-//});
+router.get('/', function(req, res, next){
+    res.status(200).json({success: true, message: "WS"});
+});
 
 //router.ws('/echo', function(ws, req) {
 //    ws.on('message', function(msg) {
